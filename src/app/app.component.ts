75c073daaa39db09acec7eb1b4006734693dc7c5
SergioEstebanPi <sepi_147@hotmail.com>
2019-08-11 22:14:25 -0500

    Agrego estructura inicial del formulario y configuro el consumo del api rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ClienteService } from './cliente.service';
+import { Cliente } from './cliente.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'banco';
+  clienteForm: Cliente;
+  existeCliente: any;
+
+  constructor(private clienteService: ClienteService){
+    this.clienteForm = {
+      identification: "",
+      firstname: "",
+      lastname: "",
+      birthdate: ""
+    };
+  }
+
+  createClient(){
+    this.clienteService.getCliente(this.clienteForm.identification)
+      .subscribe(
+        respuesta => {
+          this.existeCliente = respuesta;
+          if(this.existeCliente.codigo == "200"){
+            alert("NO es posible crear el cliente");
+          } else {
+            alert("Se realiza el registro del cliente");
+            this.clienteService.createClient(this.clienteForm)
+            .subscribe();      
+          }
+        },
+        error => {
+          alert("Se ha producido un error al crear el cliente");
+        });
+  }
 }
