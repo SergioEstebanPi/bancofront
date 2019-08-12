1559ef0c1fc5e59bbfdc8a8412b13bddec7f9646
SergioEstebanPi <sepi_147@hotmail.com>
2019-08-11 23:07:07 -0500

    Ajusto campos del formulario y agrego swal para mostrar mensaje de alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { ClienteService } from './cliente.service';
 import { Cliente } from './cliente.model';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,10 @@ export class AppComponent {
   title = 'banco';
   clienteForm: Cliente;
   existeCliente: any;
+  errores: any;
 
   constructor(private clienteService: ClienteService){
+    this.errores = {};
     this.clienteForm = {
       identification: "",
       firstname: "",
@@ -28,15 +31,24 @@ export class AppComponent {
         respuesta => {
           this.existeCliente = respuesta;
           if(this.existeCliente.codigo == "200"){
-            alert("NO es posible crear el cliente");
+            //alert("NO es posible crear el cliente");
+            swal("Error", 
+              "NO es posible crear el cliente",
+              "error");
           } else {
-            alert("Se realiza el registro del cliente");
+            //alert("Se realiza el registro del cliente");
             this.clienteService.createClient(this.clienteForm)
-            .subscribe();      
+            .subscribe();
+            swal("Cliente registrado", 
+              "Se realiza el registro del cliente",
+              "success");            
           }
         },
         error => {
-          alert("Se ha producido un error al crear el cliente");
+          //alert("Se ha producido un error al consultar el cliente");
+          swal("Error", 
+          "Se ha producido un error al consultar el cliente",
+          "error");           
         });
   }
 }
